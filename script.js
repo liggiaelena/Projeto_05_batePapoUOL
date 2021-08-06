18f8cb05e2ed178c77d3381d8cb9ced1c02b4b04
liggia <liggia_elena@usp.br>
2021-08-06 14:53:40 -0300

    layout meio pronto

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-function(){
-    
+function enviarMenssagem(){
+    let textoInput = document.querySelector("input").value;
+    document.querySelector("input").reset;
+    let corpo = document.querySelector(".corpo");
+    let menssagem = corpo.innerHTML;
+
+    menssagem += `<div class="caixa branco">
+    ${textoInput}
+    </div>`;
+
+    corpo.innerHTML = menssagem;
+    textoInput = ""; 
 }
