09ca24dc6c899ebb222b40a26617039e0eecb6b1
liggia <liggia_elena@usp.br>
2021-08-08 15:21:09 -0300

    capturei as menssagens e enviar menssagem

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 
 
 
-escolhaNome();
+//escolhaNome();
+iniciarSala();
+let menssagens="";
+let nome= "euuu";
 
 function escolhaNome() {
-   let nome = prompt("Por favor para se cadastrar digite seu nome:");
-    verificarNome(nome);
+   nome = prompt("Por favor para se cadastrar digite seu nome:");
+    verificarNome();
 }
-function verificarNome(nome){
+function verificarNome(){
     let promise = axios.post('https://mock-api.bootcamp.respondeai.com.br/api/v3/uol/participants',{nome});
     promise.then(iniciarSala);
-    //promise.catch(quandoErro);   
+    promise.catch(quandoErro);   
 }
 
 function quandoErro(erro){
     console.log(erro.response);
-    let nome;
     if(erro.response.status === 400){
        nome = prompt("Esse nome já foi escolhido, por favor digite outro:");
+       console.log(erro)
     }
 
     verificarNome(nome);
@@ -31,31 +34,51 @@ function iniciarSala(){
 }
 
 function carregarMenssagens(menssagens){
-   let corpo = document.querySelector(".corpo");
-    let menssagem = corpo.innerHTML;
+    let corpo = document.querySelector(".corpo");
+     menssagem = corpo.innerHTML;
+ 
+     for(let i=0; i<menssagens.data.length; i++){
+         let tipo = menssagens.data[i].type;
+        construirMenssagem(menssagens,tipo,i); 
+     }
 
-    for(let i=0; i<menssagens.data.length; i++){
-        console.log("hey")
-        let tipo = menssagens.data[i].type;
-        let deQuem = menssagens.data[i].from;
-        let paraQuem = menssagens.data[i].to;
-        let texto = menssagens.data[i].text;
-        let horario = menssagens.data[i].time;
-        
-        menssagem += `<div class="caixa ${tipo}"><span class="horario">(${horario})</span><span class="nome">${deQuem}</span>para<span class="nome">${paraQuem}</span>: ${texto}</div>`;
+     corpo.innerHTML = menssagem;
+     corpo.lastChild.scrollIntoView();
+     
+ }
+
+function construirMenssagem(menssagens,tipo,i){
+         let deQuem = menssagens.data[i].from;
+         let paraQuem = menssagens.data[i].to;
+         let texto = menssagens.data[i].text;
+         let horario = menssagens.data[i].time;
+    if (tipo === "status"){
+        menssagem += `<div class="caixa ${tipo}"><span class="horario">(${horario})</span><strong>${deQuem}</strong>${texto}</div>`    
     }
-    corpo.innerHTML = menssagem;
+
+    if(tipo === "message"){
+        menssagem += `<div class="caixa ${tipo}"><span class="horario">(${horario})</span><strong>${deQuem}</strong> para <strong>${paraQuem}</strong>: ${texto}</div>`  
+    }
+
+    if(tipo === "private_message"){
+        menssagem += `<div class="caixa ${tipo}"><span class="horario">(${horario})</span><strong>${deQuem}</strong> reservadamente para <strong>${paraQuem}</strong>: ${texto}</div>`    
+    }
 }
+
 
 function enviarMenssagem(){
     let textoInput = document.querySelector("input").value;
     let corpo = document.querySelector(".corpo");
     let menssagem = corpo.innerHTML;
+    let data = new Date();
+    let hora = data.getHours(); 
+    let min = data.getMinutes();        
+    let seg = data.getSeconds(); 
+    let horario = `(${hora}:${min}:${seg})`;
 
-    menssagem += `<div class="caixa message">
-    ${textoInput}
-    </div>`;
+    menssagem += `<div class="caixa message"><span class="horario">${horario}</span><strong>${nome}</strong> para <strong>Todos</strong>: ${textoInput}</div>`;
 
     corpo.innerHTML = menssagem;
+    corpo.lastChild.scrollIntoView();
     textoInput = ""; 
 }
